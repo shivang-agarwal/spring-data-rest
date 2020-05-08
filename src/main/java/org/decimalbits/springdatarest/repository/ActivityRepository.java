3f85d16ab8c6af96e1a5373d7ae1dba22e964542
shivagarwal <v-shivagarwal@expedia.com>
2020-05-08 14:50:52 +0530

    adding search by target date.

--- src/main/java/org/decimalbits/springdatarest/repository/ActivityRepository.java
+++ src/main/java/org/decimalbits/springdatarest/repository/ActivityRepository.java
@@ -2,8 +2,10 @@ package org.decimalbits.springdatarest.repository;
 
 import org.decimalbits.springdatarest.model.Activity;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "activities", collectionResourceRel = "activities")
 public interface ActivityRepository extends PagingAndSortingRepository<Activity, String> {
+    public Activity findByTargetDate(@Param("date") String targetDate);
 }
